e741734d36791b9187fdb1df2776dccaa86b986b
msgalvan1207 <ms.galvan@uniandes.edu.co>
2025-04-25 00:47:16 -0500

    refact: making the code prettier

--- sasConnection/sasCon.py
+++ sasConnection/sasCon.py
@@ -1,11 +1,9 @@
-import bcd
 import serial
 import sys
 import binascii
 import time
 
-from PYCRC.CRC16Kermit import CRC16Kermit
-from array import array
+from PyCRC.CRC16Kermit import CRC16Kermit
 
 import utilities.stateGenerators as gens
 
@@ -50,93 +48,84 @@ class SasConnection():
     def machineIDAndInformation(self):
         #1F
         cmd = [0x1F]
-        data = self.__sendCommand(cmd, true, crc_need = False)
+        data = self.__sendCommand(cmd, True, crc_need = False)
         if data != '':
-            self.meters['ASCII_game_ID']=(((data[1:3])))
-            self.meters['ASCII_additional_ID']=(((data[3:6])))
-            self.meters['bin_denomination']=int(binascii.hexlify(bytearray(data[6])))
-            self.meters['bin_max_bet']=(binascii.hexlify(bytearray(data[7:8])))
-            self.meters['bin_progressive_mode']=int(binascii.hexlify(bytearray(data[8:9])))
-            self.meters['bin_game_options']=(binascii.hexlify(bytearray(data[9:11])))
-            self.meters['ASCII_paytable_ID']=(((data[11:17])))
-            self.meters['ASCII_base_percentage']=(((data[17:21])))
+            self.meters['ASCII_game_ID'] = data[1:3].decode(errors='ignore')
+            self.meters['ASCII_additional_ID'] = data[3:6].decode(errors='ignore')
+            self.meters['bin_denomination'] = self.__from_bcd(data[6:7])
+            self.meters['bin_max_bet'] = self.__from_bcd(data[7:8])
+            self.meters['bin_progressive_mode'] = self.__from_bcd(data[8:9])
+            self.meters['bin_game_options'] = self.__from_bcd(data[9:11])
+            self.meters['ASCII_paytable_ID'] = data[11:17].decode(errors='ignore')
+            self.meters['ASCII_base_percentage'] = data[17:21].decode(errors='ignore')
     
     def meters_11_15(self):
         #19
         cmd = [0x19]
         data = self.__sendCommand(cmd, True, crc_need = False)
         if data != '':
-            self.meters['total_bet_meter']=int(binascii.hexlify(bytearray(data[1:5])))
-            self.meters['total_win_meter']=int(binascii.hexlify(bytearray(data[5:9])))
-            self.meters['total_in_meter']=int(binascii.hexlify(bytearray(data[9:13])))
-            self.meters['total_jackpot_meter']=int(binascii.hexlify(bytearray(data[13:17])))
-            self.meters['games_played_meter']=int(binascii.hexlify(bytearray(data[17:21])))
+            self.meters['total_bet_meter'] = self.__from_bcd(data[1:5])
+            self.meters['total_win_meter'] = self.__from_bcd(data[5:9])
+            self.meters['total_in_meter'] = self.__from_bcd(data[9:13])
+            self.meters['total_jackpot_meter'] = self.__from_bcd(data[13:17])
+            self.meters['games_played_meter'] = self.__from_bcd(data[17:21])
 
 
     def __sendCommand(self, command, no_response=False, timeout=3, crc_need=True):
-        busy = True
         response = b''
         try:
-            buf_header = [self.address]
-            buf_header.extend(command)
-            buf_count = len(command)
-
-            if (crc_need):
-                crc=CRC16Kermit().calculate(str(bytearray(buf_header)))
-                buf_header.extend([((crc>>8)&0xFF),(crc&0xFF)])
+            packet = bytearray([self.address] + command)
+            if crc_need:
+                crc= self.__calculate_crc(packet)
+                packet.extend([(crc >> 8) & 0xFF, crc & 0xFF])
             
-            print(buf_header)
+            print(f"Sending: {binascii.hexlify(packet)}")
+            self.connection.write(packet)
 
-            self.connection.write((buf_header))
-        
-        except Exception as e:
-            print(e)
-        
-        try:
-            buffer = []
-            self.connection.flushInput()
-            t = time.time()
-            while time.time() - t<timeout:
-                response += self.connection.read()
-
-                if self.checkResponse != False:
-                    break
+            if no_response:
+                return b''
             
-            if time.time() - t >= timeout:
-                print("timeout waiting response")
-                return None
-            busy = False
-            return self.checkResponse(response)
-        
+            self.connection.reset_input_buffer()
+            start_time = time.time()
+
+            while time.time() - start_time < timeout:
+                response += self.connection.read(1)
+                parsed = self.checkResponse(response)
+                if parsed:
+                    return parsed
+            
+            print("Timeout waiting for response")
+            return None
         except Exception as e:
-            print(e)
-        busy = False
-        return None
+            print(f"Error in __sendCommand: {e}")
+            return None
+            
+    
+
+    def __calculate_crc(self, data_bytes):
+        return CRC16Kermit().calculate(data_bytes)
     
     def checkResponse(self, rsp):
-        if (rsp==''):
+        if not rsp:
             return False
+
         resp = bytearray(rsp)
+        if resp[0] != self.address:
+            raise ValueError(f"Unexpected address: {resp[0]} (expected {self.address})")
 
-        if (resp[0] != self.address):
-            raise Exception("incorrect adress catched in serial connection")
-        
-        CRC = binascii.hexlify(resp[-2:])
-
-        command = resp[0:-2]
-
-        crc1=crc=CRC16Kermit().calculate(str(bytearray(command)))
-
+        received_crc = resp[-2:]
         data = resp[1:-2]
+        crc_calc = self.__calculate_crc(resp[:-2])
 
-        crc1 = hex(crc1).split('x')[-1]
+        crc_bytes = bytearray([(crc_calc >> 8) & 0xFF, crc_calc & 0xFF])
 
-        while len(crc1)<4:
-                        crc1 = "0"+crc1
-        
-        if CRC != crc1:
+        if received_crc != crc_bytes:
+            print(f"CRC mismatch: received {binascii.hexlify(received_crc)}, expected {binascii.hexlify(crc_bytes)}")
             return False
-        
+
         return data
+    
+    def __from_bcd(self, data):
+        return int(binascii.hexlify(data),16)
 
 
